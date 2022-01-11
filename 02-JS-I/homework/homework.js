76f61ace3aed1ae872afc943bc5604b842d90f03
your name <you@example.com>
2022-01-11 12:08:45 -0300

    modified:

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -196,7 +196,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "hola " + nombre + "!"  ;
+  return "hola" + nombre + "!" ; 
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
